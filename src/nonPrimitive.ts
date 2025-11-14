f441dd9a53bf1110c3f2d29786c38429d48db0d9
learn-git <alaminhossen176466@gmail.com>
2025-11-14 20:33:59 +0600

    create simple primitive & non-primitive structuer

---
+++ src/nonPrimitive.ts
@@ -0,0 +1,21 @@
+/* array , object */
+
+// true 
+// false
+
+let list: string[] = ['eggs', "milk" ,  "sugar", "potato"];
+
+list.push("12")
+
+let mixedArray : (string | number)[] = ['eggs', 'milk', 'sugar', 1]
+
+mixedArray.push('tomato', 2)
+
+
+let coordinates  : [number, number] = [20, 30, 34];
+
+let couple  : [string, string] = ['harun', "wife"] ;
+
+
+let nameAndRoll : [string, number] = ['almamin', 70];
+let destination  :[string, string, number]= ['Dhaka', 'cumilla', 2];
---
+++ src/primitive.ts
@@ -0,0 +1,10 @@
+//  js - ts 
+// string , number , boolean , undefined, null
+
+let userName : string = "alamin";
+
+let userId = 134
+userId.toFixed()
+
+let isAdmin = false;
+ isAdmin = true;
